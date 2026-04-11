2e4277081a3c66b390fb1b9e0e7aa1a5d99ca791
FanyRM <celeste.e.ramirez.m@gmail.com>
2026-04-10 18:38:29 -0600

    Cambios

--- backend/config.py
+++ backend/config.py
@@ -16,7 +16,7 @@ class Config:
     DB_PORT = os.getenv("DB_PORT", "3306")
     DB_NAME = os.getenv("DB_NAME", "neveriapos")
     DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
